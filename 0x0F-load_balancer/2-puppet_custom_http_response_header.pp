c3a68f854ca916805f903a0ae466cd553a233e36
Palesapp <nnupalesa.pp@gmail.com>
2024-03-29 07:37:56 -0700

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,28 +1,30 @@
-# Puppet manifest creating a custom HTTP header response.
+# Setup New Ubuntu server with nginx
+# and add a custom HTTP header
 
-exec { 'exec_0':
-  command => 'sudo sudo apt-get update -y',
-  path    => ['/usr/bin', '/bin'],
-  returns => [0,1]
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
-exec { 'exec_1':
-  require => Exec['exec_0'],
-  command => 'sudo apt-get install nginx -y',
-  path    => ['/usr/bin', '/bin'],
-  returns => [0,1]
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-exec { 'exec_2':
-  require => Exec['exec_1'],
-  command => 'sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-enabled/default',
-  path    => ['/usr/bin', '/bin'],
-  returns => [0,1]
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-exec { 'exec_3':
-  require => Exec['exec_2'],
-  command => 'sudo service nginx start',
-  path    => ['/usr/bin', '/bin', '/usr/sbin'],
-  returns => [0,1]
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
